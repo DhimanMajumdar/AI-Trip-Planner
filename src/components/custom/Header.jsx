405fd1e773d691bf9146a9ed3f334678a11b7ae1
DhimanMajumdar <dhimanmajumdar08233@gmail.com>
2025-02-18 23:37:00 +0530

    Completed header and hero section

--- src/components/custom/Header.jsx
+++ src/components/custom/Header.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import { Button } from '../ui/button'
+import React from "react";
+import { Button } from "../ui/button";
 
 const Header = () => {
   return (
-    <div className='p-3 shadow-lg flex justify-between items-center px-5 w-full'>
-      <img src='/logo.svg'/>
+    <div className="p-3 shadow-lg flex justify-between items-center px-5 w-full">
+      <img src="/logo.svg" />
       <div>
         <Button>Sign In</Button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
